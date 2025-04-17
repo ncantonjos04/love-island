0782bae72b24207f66ca222614262f1822db33a3
Nadine Angela Cantonjos <ncantonjos04@gmail.com>
2025-04-17 17:56:31 -0400

    Putting everything in the root directory for deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function insertCastDiv (name, last_name){
     document.querySelector('.cast-grid').innerHTML += `
     <a href = "https://loveisland.fandom.com/wiki/${name}_${last_name}">
         <div class = "contestant">
-                <img class = "cast-pic" src = "./media/${name}.jpg">
+                <img class = "cast-pic" src = "media/${name}.jpg">
                 <p class = "name">${name}</p>
         </div>
     </a>
